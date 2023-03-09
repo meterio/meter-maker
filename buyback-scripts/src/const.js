ace8211aaef36b79e846a7116b88a832adb49c23
Simon.Z <zhanghan.simon@gmail.com>
2023-03-08 17:15:30 -0800

    update buyback-scripts to use the new meterMaker

--- buyback-scripts/src/const.js
+++ buyback-scripts/src/const.js
@@ -1,7 +1,8 @@
 module.exports = {
-  rpcUrl: 'http://mainnet.meter.io',
-  metermakerAddr : "0x71fcec7bdf90c0aad2d9fec48bb9507e067a304f",
-  meterAddr : '0x687a6294d0d6d63e751a059bf1ca68e4ae7b13e2',
-  reserve : 0.2e18,
-  minMTRToTransfer: 0.1e18
+  rpcUrl: "http://mainnet.meter.io",
+  // metermakerAddr : "0x71fcec7bdf90c0aad2d9fec48bb9507e067a304f",
+  metermakerAddr: "0xd62881db74971ad821522dfb755bc87611acc225",
+  meterAddr: "0x687a6294d0d6d63e751a059bf1ca68e4ae7b13e2",
+  reserve: 0.2e18,
+  minMTRToTransfer: 0.1e18,
 };
